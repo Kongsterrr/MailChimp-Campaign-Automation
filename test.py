93737ad39ad85a4d302d99095fedf5ff1f9fbe3d
Jack <jackkong@bu.edu>
2024-08-15 15:12:49 -0400

    Remove test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-word = "Spectators take photos of giant panda Yun Chuan in the Panda Ridge enclosure at the San Diego Zoo in San Diego, "
-word1 = "Visitors take photos of the flying car developed by Xpeng Aeroht, which is affiliated with automaker Xpeng, at the"
-word2 = "China’s table tennis player Fan Zhendong in action during the men’s singles final at the Paris Olympics on Sunday."
-word3 = "new pandas Yun Chuan and Xin Bao on Thursday as they greeted the public at their new home in the San Diego"
-print(len("LIU GUANGUAN / CHINA NEWS SERVICE"))
-print(219 % 109 + 33)
-print(len(word3))
