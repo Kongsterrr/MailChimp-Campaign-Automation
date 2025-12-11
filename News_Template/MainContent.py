83ae88055c47e8f7f56306ea49ebb5a932d53769
Jack <jackkong@bu.edu>
2025-12-11 17:07:00 -0500

    fix: fixed padding for image

--- News_Template/MainContent.py
+++ News_Template/MainContent.py
@@ -14,12 +14,12 @@ def MainSection(news):
 
     # 👉 the block you want to insert as the 2nd item
     injected_second_block = """
-<tr><td style="background-color:transparent;padding-top:12px;padding-bottom:12px;padding-right:0;padding-left:0;border:0;border-radius:0" valign="top" class="mceImageBlockContainer" align="center" id="b132"><div><!--[if !mso]><!--></div><a href="https://www.chineseculturalfoundation.org/" style="display:block" target="_blank" data-block-id="132"><table align="center" border="0" cellpadding="0" cellspacing="0" width="85%" style="border-collapse:separate;margin:0;vertical-align:top;max-width:85%;width:85%;height:auto" role="presentation" data-testid="image-132"><tbody><tr><td style="border:0;border-radius:0;margin:0" valign="top"><img alt="" src="https://mcusercontent.com/ba1a72c67a1a445629b16aaf2/images/d57765de-cbe1-a10e-66e8-2cd832108577.png" width="561" height="auto" style="display:block;max-width:100%;height:auto;border-radius:0" class="imageDropZone mceImage"></td></tr></tbody></table></a><div><!--<![endif]--></div><div>
-<!--[if mso]>
-<a href="https://www.chineseculturalfoundation.org/"><span class="mceImageBorder" style="border:0;border-width:2px;vertical-align:top;margin:0"><img role="presentation" class="imageDropZone mceImage" src="https://mcusercontent.com/ba1a72c67a1a445629b16aaf2/images/d57765de-cbe1-a10e-66e8-2cd832108577.png" alt="" width="561" height="auto" style="display:block;max-width:561px;width:561px;height:auto"/></span></a>
-<![endif]-->
-</div></td></tr>
-""".strip()
+    <tr><td style="background-color:transparent;padding-top:12px;padding-bottom:12px;padding-right:0;padding-left:0;border:0;border-radius:0" valign="top" class="mceImageBlockContainer" align="center" id="b132"><div><!--[if !mso]><!--></div><a href="https://www.chineseculturalfoundation.org/" style="display:block" target="_blank" data-block-id="132"><table align="center" border="0" cellpadding="0" cellspacing="0" width="85%" style="border-collapse:separate;margin:0;vertical-align:top;max-width:85%;width:85%;height:auto" role="presentation" data-testid="image-132"><tbody><tr><td style="border:0;border-radius:0;margin:0" valign="top"><img alt="" src="https://mcusercontent.com/ba1a72c67a1a445629b16aaf2/images/d57765de-cbe1-a10e-66e8-2cd832108577.png" width="561" height="auto" style="display:block;max-width:100%;height:auto;border-radius:0" class="imageDropZone mceImage"></td></tr></tbody></table></a><div><!--<![endif]--></div><div>
+    <!--[if mso]>
+    <a href="https://www.chineseculturalfoundation.org/"><span class="mceImageBorder" style="border:0;border-width:2px;vertical-align:top;margin:0"><img role="presentation" class="imageDropZone mceImage" src="https://mcusercontent.com/ba1a72c67a1a445629b16aaf2/images/d57765de-cbe1-a10e-66e8-2cd832108577.png" alt="" width="561" height="auto" style="display:block;max-width:561px;width:561px;height:auto"/></span></a>
+    <![endif]-->
+    </div></td></tr>
+    """.strip()
 
     main_news = [item for item in news['News'] if item.get("Section") == "Main"]
     divider_bold = news.get("DividerBold", True)
@@ -50,7 +50,7 @@ def MainSection(news):
         title_html = (
             f'<tr><td style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0" valign="top">'
             f'<table width="100%" style="border:0;border-radius:0;border-collapse:separate">'
-            f'<tbody><tr><td style="padding-left:50px;padding-right:50px;padding-top:10px;padding-bottom:5px" class="mceTextBlockContainer">'
+            f'<tbody><tr><td style="padding-left:50px;padding-right:50px;padding-top:5px;padding-bottom:5px" class="mceTextBlockContainer">'
             f'<div data-block-id="{i}" class="mceText" id="dataBlockId-{i}" style="width:100%">'
             f'<h1 style="line-height: 1.25;" class="last-child">'
             f'<span style="font-size: 25px"><span style="font-family: \'Times New Roman\', Times, Baskerville, Georgia, serif">{item["Title"]}</span></span>'
@@ -153,7 +153,7 @@ def MainSection(news):
                                             <td style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0" valign="top" id="blockContainerId-{i}">
                                                 <table width="100%" style="border:0;border-radius:0;border-collapse:separate">
                                                     <tbody><tr>
-                                                        <td style="padding-left:50px;padding-right:0;padding-top:5px;padding-bottom:5px" class="mceTextBlockContainer">
+                                                        <td style="padding-left:50px;padding-right:0;padding-top:0;padding-bottom:5px" class="mceTextBlockContainer">
                                                             <div data-block-id="{i}" class="mceText" id="dataBlockId-{i}" style="width:100%">
                                                                 <p class="last-child">
                                                                     {text_before_link}<a href="{item["Content_Link"]}" target="_blank">{item["Content_TextToLink"]}</a>{text_after_link}
@@ -171,7 +171,7 @@ def MainSection(news):
                                 <td style="padding-top:0;padding-bottom:0" valign="top" class="mceColumn" data-block-id="{i}" colspan="6" width="50%">
                                     <table border="0" cellpadding="0" cellspacing="0" width="100%" role="presentation">
                                         <tbody><tr>
-                                            <td style="padding-top:5px;padding-bottom:5px;padding-right:50px;padding-left:50px" valign="top" class="mceImageBlockContainer" align="center" id="blockContainerId-{i}">
+                                            <td style="padding-top:10px;padding-bottom:5px;padding-right:50px;padding-left:50px" valign="top" class="mceImageBlockContainer" align="center" id="blockContainerId-{i}">
                                                 <span class="mceImageBorder" style="border:0;border-radius:0;vertical-align:top;margin:0">
                                                     <img data-block-id="{i}" width="195.5" height="auto" style="width:195.5px;height:auto;max-width:195.5px !important;border-radius:0;display:block" alt="" src="{item_image}" role="presentation" class="imageDropZone mceImage">
                                                 </span>
@@ -184,7 +184,7 @@ def MainSection(news):
             final_html = title_html + f"""
                             <tr><td valign="top" class="mceGutterContainer" id="gutterContainerId-{i}">
                                 <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse:separate" role="presentation">
-                                    <tbody><tr><td style="padding-top:8px;padding-bottom:8px;padding-right:0;padding-left:0" valign="top" class="mceLayoutContainer" id="blockContainerId-{i}">
+                                    <tbody><tr><td style="padding-top:5px;padding-bottom:5px;padding-right:0;padding-left:0" valign="top" class="mceLayoutContainer" id="blockContainerId-{i}">
                                         <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" role="presentation" data-block-id="{i}" id="section_{i}" class="mceLayout">
                                             <tbody><tr class="mceRow">
                                                 <td style="background-position:center;background-repeat:no-repeat;background-size:cover" valign="top">
